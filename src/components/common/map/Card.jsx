3ed88777d098df06674d3ad6e118084f660cb391
vidihawk <fabian@demortier.net>
2022-12-24 18:21:26 +0000

    made the content of popup card dynamic

--- src/components/common/map/Card.jsx
+++ src/components/common/map/Card.jsx
@@ -1,43 +1,88 @@
-import { motion } from "framer-motion";
+import $ from "jquery";
 
-export default function Card({ name, address, stars, url, imgUrl }) {
-  return (
-    <motion.div
-      className="absolute bottom-6 left-0 right-0 mx-auto my-0 w-80"
-      initial={{ scale: 0 }}
-      animate={{ scale: 1 }}
-      exit={{ scale: 0 }}
-    >
-      <div className="flex gap-2 p-2 rounded-lg shadow-xl bg-zinc-800 text-white">
-        <div className="w-28">
-          <p>
-            <img src={imgUrl} alt={`Cat for ${name}`} className="rounded" />
-          </p>
+export default function customCard(e) {
+  const capacity = e.features[0].properties.capacity_mw;
+  const fuel = e.features[0].properties.primary_fuel;
+  const name = e.features[0].properties.name;
+  const owner = e.features[0].properties.owner;
+  const commissioned = e.features[0].properties.commissioning_year;
+
+  const getImage = () => {
+    if (fuel == "Hydro") {
+      return "assets/images/avatar/hydro.jpg";
+    }
+    if (fuel == "Coal") {
+      return "assets/images/avatar/coal.jpg";
+    }
+    if (fuel == "Wind") {
+      return "assets/images/avatar/wind.jpg";
+    }
+    if (fuel == "Solar") {
+      return "assets/images/avatar/solar.jpg";
+    } else return "assets/images/energy-icon.svg";
+  };
+
+  const image = getImage();
+
+  return $(`
+    <div class="project-wrap">
+      <div class="project-content">
+        <div class="project-img">
+          <img src=${image} />
         </div>
-        <div className="grow">
-          <h1 className="font-bold">
-            {url && (
-              <a
-                href={url}
-                className="text-sky-400"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {name}
-              </a>
-            )}
-            {!url && name}
-          </h1>
-          <p className="text-sm">{address}</p>
-          <div className="flex items-center gap-1 text-sm text-amber-400">
-            {/* {Array.from({ length: Math.trunc(stars) }).map((_, i) => (
-              <ImStarFull key={i} />
-            ))}
-            {stars !== Math.trunc(stars) && <ImStarHalf />} */}
-            <span className="text-white">{stars}</span>
-          </div>
-        </div>
+        <h3 class="project-title">${capacity} MW ${name}</h3>
+        <p class="project-address">
+          Owner: ${owner}
+        </p>
+        <p class="project-telephone">
+          Year: ${commissioned}
+        </p>
       </div>
-    </motion.div>
-  );
+    </div>
+    `);
+}
+
+// ALTRENATIVE POPUP CARD STYLED WITH FRAME-MOTION PACKAGE
+
+{
+  /* 
+  import { motion } from "framer-motion";
+  
+  <motion.div
+className="absolute bottom-6 left-0 right-0 mx-auto my-0 w-80"
+initial={{ scale: 0 }}
+animate={{ scale: 1 }}
+exit={{ scale: 0 }}
+>
+<div className="flex gap-2 p-2 rounded-lg shadow-xl bg-zinc-800 text-white">
+  <div className="w-28">
+    <p>
+      <img src={imgUrl} alt={`Cat for ${name}`} className="rounded" />
+    </p>
+  </div>
+  <div className="grow">
+    <h1 className="font-bold">
+      {url && (
+        <a
+          href={url}
+          className="text-sky-400"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {name}
+        </a>
+      )}
+      {!url && name}
+    </h1>
+    <p className="text-sm">{address}</p>
+    <div className="flex items-center gap-1 text-sm text-amber-400">
+      {/* {Array.from({ length: Math.trunc(stars) }).map((_, i) => (
+        <ImStarFull key={i} />
+      ))}
+      {stars !== Math.trunc(stars) && <ImStarHalf />} 
+      <span className="text-white">{stars}</span>
+    </div>
+  </div>
+</div>
+</motion.div> */
 }
--- src/components/common/map/initializeMap.js
+++ src/components/common/map/initializeMap.js
@@ -1,4 +1,4 @@
-import $ from "jquery";
+import customCard from "./Card";
 
 export function initializeMap(mapboxgl, map) {
   map.on("click", "data", function (e) {
@@ -17,58 +17,36 @@ export function initializeMap(mapboxgl, map) {
       });
   });
 
-  const customPop = $(`
-  <div class="project-wrap">
-    <div class="project-content">
-      <div class="project-img">
-        <img src="assets/images/generators/american-public-power-association-513dBrMJ_5w-unsplash.jpg" />
-      </div>
-      <h3 class="project-title">15 MW  Hartford Power</h3>
-      <p class="project-address">
-        <i class="fa-solid fa-location-dot"></i> Wexford road,
-        County Wicklow, Ireland
-      </p>
-      <p class="project-telephone">
-        </i> Solar
-      </p>
-    </div>
-  </div>
+  // const bla = 20;
 
-  `);
+  // const customPop = $(`
+  // <div class="project-wrap">
+  //   <div class="project-content">
+  //     <div class="project-img">
+  //       <img src="assets/images/generators/american-public-power-association-513dBrMJ_5w-unsplash.jpg" />
+  //     </div>
+  //     <h3 class="project-title">${bla} MW  Hartford Power</h3>
+  //     <p class="project-address">
+  //       <i class="fa-solid fa-location-dot"></i> Wexford road,
+  //       County Wicklow, Ireland
+  //     </p>
+  //     <p class="project-telephone">
+  //       </i> Solar
+  //     </p>
+  //   </div>
+  // </div>
+  // `);
 
   map.on("click", "unclustered-point-wind", function (e) {
     const coordinates = e.features[0].geometry.coordinates.slice();
-    const fuel = e.features[0].properties.primary_fuel;
-    const name = e.features[0].properties.name;
-    const owner = e.features[0].properties.owner;
-    const commissioned = e.features[0].properties.commissioning_year;
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
       coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
+    const customPop = customCard(e);
 
-    // customPop.find(".custommarker").click(function () {
-    //   $(this).parent().toggleClass("open");
-    // });
-    // customPop.find(".close").click(function () {
-    //   $(this).closest(".marker-wrapper").removeClass("open");
-    // });
     new mapboxgl.Marker(customPop[0])
       .setLngLat([coordinates[0] + 2.2, coordinates[1] - 1])
       .addTo(map);
-
-    // new mapboxgl.Popup({ className: "blabla" })
-    //   .setLngLat(coordinates)
-    //   .setHTML(
-    //     "Name: " +
-    //       name +
-    //       "<br>Fuel: " +
-    //       fuel +
-    //       "<br>Owner: " +
-    //       owner +
-    //       "<br>Commissioned in " +
-    //       commissioned
-    //   )
-    //   .addTo(map);
   });
 
   map.on("mouseenter", "unclustered-point-wind", function () {
@@ -90,22 +68,16 @@ export function initializeMap(mapboxgl, map) {
   map.addControl(new mapboxgl.FullscreenControl());
 }
 
-{
-  /* <motion.div
-initial={{ opacity: 0 }}
-animate={{ opacity: 1, transition: { delay: Math.random() * 0.3 } }}
-exit={{ opacity: 0 }}
-transition={{
-  type: "spring",
-  stiffness: 400,
-  damping: 20,
-}}
->
-<button
-  className={`rounded-full bg-zinc-600 py-1.5 px-2 drop-shadow text-xs text-white ${
-    highlight && "text-black bg-zinc-50 font-bold py-2 px-2.5"
-  }`}
-  onClick={handleClick}
->{`$ ${price}`}</button>
-</motion.div>  */
-}
+// new mapboxgl.Popup({ className: "blabla" })
+//   .setLngLat(coordinates)
+//   .setHTML(
+//     "Name: " +
+//       name +
+//       "<br>Fuel: " +
+//       fuel +
+//       "<br>Owner: " +
+//       owner +
+//       "<br>Commissioned in " +
+//       commissioned
+//   )
+//   .addTo(map);
