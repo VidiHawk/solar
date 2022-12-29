09aed1b07008b8dd458520607da8d16169ca1a63
vidihawk <fabian@demortier.net>
2022-12-29 13:42:00 +0000

    marker changes position on zoom in or out - fixed

--- src/components/common/map/initializeMap.js
+++ src/components/common/map/initializeMap.js
@@ -47,13 +47,40 @@ export function initializeMap(mapboxgl, map, setSharedCard, setSelectedMarker) {
     map.on("mouseenter", id, function (e) {
       map.getCanvas().style.cursor = "pointer";
       const [coordinates, customPop] = [getCoordinates(e), customCard(e)];
-      // const coordinates = getCoordinates(e);
-      const card = new mapboxgl.Marker(customPop[0]).setLngLat([
-        coordinates[0],
-        coordinates[1] - 0.1,
-      ]);
+      const zoom = map.getZoom();
+      const lng =
+        zoom <= 3
+          ? coordinates[0] + 15
+          : 3 < zoom && zoom <= 4
+          ? coordinates[0] + 5
+          : 4 < zoom && zoom <= 5
+          ? coordinates[0] + 2.5
+          : 5 < zoom && zoom <= 6
+          ? coordinates[0] + 1.5
+          : 6 < zoom && zoom <= 8
+          ? coordinates[0] + 0.4
+          : 8 < zoom && zoom <= 11
+          ? coordinates[0] + 0.02
+          : coordinates[0] + 0.005;
+      const lat =
+        zoom <= 3
+          ? coordinates[1] - 10
+          : 3 < zoom && zoom <= 4
+          ? coordinates[1] - 3
+          : 4 < zoom && zoom <= 5
+          ? coordinates[1] - 1.5
+          : 5 < zoom && zoom <= 6
+          ? coordinates[1] - 0.8
+          : 6 < zoom && zoom <= 8
+          ? coordinates[1] - 0.1
+          : 8 < zoom && zoom <= 11
+          ? coordinates[1] - 0.02
+          : coordinates[1] - 0.005;
+      -0.005;
+      const card = new mapboxgl.Marker(customPop[0]).setLngLat([lng, lat]);
       card.addTo(map);
       cardData.push(card);
+      // const coordinates = getCoordinates(e);
       // cardData.push([coordinates, getCardData(e)]);
       // setSharedCard(cardData);
     });
